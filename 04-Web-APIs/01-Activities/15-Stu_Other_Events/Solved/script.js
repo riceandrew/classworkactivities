4d3ec6378912f85b8f872bbd3e3bd31111905d5c
Andrew Rice <riceandrew8@gmail.com>
2019-11-02 10:19:16 -0500

    updates

--- 04-Web-APIs/01-Activities/15-Stu_Other_Events/Solved/script.js
+++ 04-Web-APIs/01-Activities/15-Stu_Other_Events/Solved/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var eventType = document.querySelector("#event-type"); 
 var mouseEventsEl = document.querySelector("#click-events");
 var keyEventsEl = document.querySelector("#key-events");
@@ -39,46 +38,4 @@ function click(event) {
 document.addEventListener("keydown", keydown);
 document.addEventListener("keyup", keyup);
 document.addEventListener("click", click);
-=======
-var eventType = document.querySelector("#event-type"); 
-var mouseEventsEl = document.querySelector("#click-events");
-var keyEventsEl = document.querySelector("#key-events");
-
-function toggleDisplay(event) {
-  var value = event.target.value;
-  if(value === "keydown") {
-    mouseEventsEl.classList.add("hide");
-    keyEventsEl.classList.remove("hide");
-  }
-  else {
-    mouseEventsEl.classList.remove("hide");
-    keyEventsEl.classList.add("hide");
-  }
-}
-
-function keydown(event) {
-  var keyPress = event.key;
-  var keyCode = event.code;
-  document.querySelector("#key").textContent = keyPress;
-  document.querySelector("#code").textContent = keyCode;
-  document.querySelector("#status").textContent = "KEYDOWN Event";
-}
-
-function keyup() {
-  document.querySelector("#status").innerHTML = "KEYUP Event";
-}
-
-function click(event) {
-  var target = event.target.textContent;
-  var x = event.clientX;
-  var y = event.clientY;
-  document.querySelector("#target").textContent = target;
-  document.querySelector("#x").textContent = x;
-  document.querySelector("#y").textContent = y;
-}
-
-document.addEventListener("keydown", keydown);
-document.addEventListener("keyup", keyup);
-document.addEventListener("click", click);
->>>>>>> 2eea7de7cd53209ef5faf8234bd2aa46e0469e46
 eventType.addEventListener("change", toggleDisplay);
